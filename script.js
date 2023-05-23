bbfe0ae9f8a917f0911155e0f79d2c1075c733ec
zephanrs <70251978+zephanrs@users.noreply.github.com>
2023-05-22 19:51:55 -0700

    Update script.js

--- script.js
+++ script.js
@@ -5,16 +5,16 @@ let operator = '';
 function addNum(num) {
   if (!operator) {
     firstNum += num;
-    document.getElementById('display').defaultValue = firstNum;
+    document.getElementById('display').value = firstNum;
   } else {
     secondNum += num;
-    document.getElementById('display').defaultValue = secondNum;
+    document.getElementById('display').value = secondNum;
   }
 }
 
 function setOperator(op) {
   operator = op;
-  document.getElementById('display').defaultValue = operator;
+  document.getElementById('display').value = operator;
 }
 
 function calculate() {
@@ -36,7 +36,7 @@ function calculate() {
   if (operator === '*') result = parseFloat(firstNum) * parseFloat(secondNum);
   if (operator === '/') result = parseFloat(firstNum) / parseFloat(secondNum);
 
-  document.getElementById('display').defaultValue = result.toString();
+  document.getElementById('display').value = result.toString();
   firstNum = result.toString();
   secondNum = '';
   operator = '';
@@ -45,6 +45,27 @@ function calculate() {
 function submitCaptcha() {
   document.getElementById('captcha').style.display = 'none';
   calculate();
+}
+
+window.onload = function() {
+  let numButtons = document.getElementsByClassName('num');
+  for(let i = 0; i < numButtons.length; i++) {
+    numButtons[i].addEventListener('click', function() {
+      addNum(numButtons[i].textContent);
+    });
+  }
+
+  let operatorButtons = document.getElementsByClassName('operator');
+  for(let i = 0; i < operatorButtons.length; i++) {
+    operatorButtons[i].addEventListener('click', function() {
+      setOperator(operatorButtons[i].dataset.op);
+    });
+  }
+
+  document.getElementById('equals').addEventListener('click', calculate);
+  document.getElementById('clear').addEventListener('click', clearCalc);
+  document.getElementById('captcha-submit').addEventListener('click', submitCaptcha);
+  document.getElementById('pay').addEventListener('click', pay);
 }
 
 function pay() {
